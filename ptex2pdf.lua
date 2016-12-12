584ef283fdc3ab75a4b01333f88852c87c7cbc22
Norbert Preining <norbert@preining.info>
2016-12-12 09:05:35 +0900

    reorder copyright to make CTAN display more adaequate

--- ptex2pdf.lua
+++ ptex2pdf.lua
@@ -28,9 +28,9 @@ options: -v  version
 LICENSECOPYRIGHT = [[
 Originally based on musixtex.lua from Bob Tennent.
 
-(c) Copyright 2012 Bob Tennent rdt@cs.queensu.ca  
-(c) Copyright 2013-2016 Norbert Preining norbert@preining.info  
 (c) Copyright 2016      by Japanese TeX Development Community  
+(c) Copyright 2013-2016 Norbert Preining norbert@preining.info  
+(c) Copyright 2012 Bob Tennent rdt@cs.queensu.ca  
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the
